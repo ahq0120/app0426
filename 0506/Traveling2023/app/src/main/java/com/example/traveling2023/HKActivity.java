0beaf5bcc4bf7ed77b86828d9a645048db6c2b6c
ahq0120 <69300774+ahq0120@users.noreply.github.com>
2023-05-06 09:58:42 +0800

    Add files via upload

---
+++ 0506/Traveling2023/app/src/main/java/com/example/traveling2023/HKActivity.java
@@ -0,0 +1,16 @@
+package com.example.traveling2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HKActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hkactivity);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
